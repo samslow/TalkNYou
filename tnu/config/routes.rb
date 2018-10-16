5331748cb94700af8afd4fad04c7f728dd4d7937
samslow <shsvkdldj@likelion.org>
2018-10-16 14:56:36 +0900

    no

--- tnu/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get '/keyboard' => 'kakao#keyboard'
-
-  get '/message' => 'kakao#message'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
