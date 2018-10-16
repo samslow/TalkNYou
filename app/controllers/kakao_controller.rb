806d17a6237dbc4121fa166d93e1dd4695330f0d
Hyeonseok seo <shsvkdldj@likelion.org>
2018-10-16 06:31:02 +0000

    prototype

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -12,20 +12,13 @@ class KakaoController < ApplicationController
     
     if @user_msg == "기념일"
       @text = "8월 28일"
-    elsif @user_msg == "로또"
-      @text = "냥냥이가 뽑은 이번주의 로또번호는..? \n" + (1..45).to_a.sample(6).sort.to_s + "이다 냥!!"
-    elsif @user_msg == "메뉴선택"
-      @text = ["한식", "일식", "중식", "양식"].sample.to_s
-    elsif @user_msg == "오늘까지몇일?"
-      @text = ((-(Date.new(2017,8,28) - Date.today)).to_i + 1).to_s + "일째 연애중♥"
-    elsif @user_msg == "앞으로의 약속"
-      @text = "7월 7일 보드게임콘테스트"
     elsif @user_msg == "오늘고양이"
       @url = "http://thecatapi.com/api/images/get?format=xml&type=jpg"
       @cat_xml = RestClient.get(@url)
       @cat_doc = Nokogiri::XML(@cat_xml) #:: 는 모듈안에 있다는 뜻 'nokogiri' 안의 xml 모듈 
       @cat_url = @cat_doc.xpath("//url").text
-      # @text = @cat_url.text
+    elsif @user_msg == "버튼 선택"
+      @text = "버튼을 선택해 닝겐"
       
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/keyboard' => 'kakao#keyboard'
 
-  get '/message' => 'kakao#message'
+  post '/message' => 'kakao#message'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
