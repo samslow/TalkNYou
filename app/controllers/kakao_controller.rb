08c336651dd9631074e163e77913cafd1cf19110
Hyeonseok seo <shsvkdldj@likelion.org>
2018-10-29 09:20:41 +0000

    유저정보 DB가져오기 milestone

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -20,37 +20,39 @@ class KakaoController < ApplicationController
     
     @user_msg = params[:content] #사용자의 입력값
     @cuser = User.find_by(key: params[:user_key])
-    @usite = []
+    @usite =  []
+    @cuser.sites.each do |s|
+      @usite.push(s.sname)
+    end
+    
+      
     
     if @user_msg == "홈으로"
       @text = "홈으로 돌아왔다능.."
-    elsif !@cuser.flag?
+      @cuser.update(flag: 0)
+    elsif @hsite.include?(@user_msg)
+      
+    
+    elsif @user_msg == "[직접입력]"
+      @text = "사이트 이름을 입력 해 주세요"
+      @cuser.update(flag: 1)
+    elsif @cuser.flag == 0
       if @user_msg == "사이트 리스트"
         @text = "[Site list]\n"
-        
-        @cuser.sites.each do |s|
-          @usite.push(s.sname)
-          # @text << s.sname << "\n"
-        end
         
         @usite.push("[추가하기]")
         
         if @cuser.sites.first
           @usite.push("[삭제하기]")
         end
-        
       elsif @user_msg == "[추가하기]"
         @text = "아래에서 추가할 사이트를 선택하거나\n 직접 입력하세요"
-        @cuser.update(flag: 1)
+        @cuser.update(flag: 1) 
       elsif @user_msg == "[삭제하기]"
         @text = "삭제 할 사이트를 선택 해 주세요"
         @cuser.sites.each do |s|
           @usite.push(s.sname)
-          # @text << s.sname << "\n"
         end
-        @cuser.update(flag: 0)
-      elsif @user_msg == "[직접입력]"
-        @text = "사이트 이름을 입력 해 주세요"
       else
         @text = "잘못된 입력이라능!"
       end
@@ -59,16 +61,15 @@ class KakaoController < ApplicationController
       @text = "["+ @user_msg + "]\n"+"거의 다 왔습니다.\n이젠 아이디를 입력 해 볼까요?"
       @cuser.update(flag: 2)# 아이디 입력 모드
       @cuser.sites.each do |s|
-          @usite.push(s.sname)
-          # @text << s.sname << "\n"
-        end
+        @usite.push(s.sname)
+      end
     elsif @cuser.flag == 2
       Site.where(user: @cuser).last.update(sid: @user_msg)
       @text = "["+ @user_msg + "]\n"+"마지막 단계입니다.\n패스워드를 입력 해 주세요"
       @cuser.update(flag: 3) # 비번 입력 모드 
     elsif @cuser.flag == 3
       Site.where(user: @cuser).last.update(spw: @user_msg)
-      @text = @user_msg + "\n저장 되었습니다.\n사이트리스트로 돌아갑니다."
+      @text ="저장 되었습니다.\n사이트리스트로 돌아갑니다."
       @cuser.update(flag: 0)
     elsif @cuser.flag == -1 && Site.where(user: @cuser, sname: @user_msg).last
       Site.where(user: @cuser, sname: @user_msg).last.destroy
@@ -113,14 +114,14 @@ class KakaoController < ApplicationController
       @cuser.update(flag: -1)
     elsif @user_msg == "[직접입력]"
       @cuser.update(flag: 1) # 사이트 이름 직접 입력 플래그 
-      p @cuser.flag
       @result = {
         message: @return_msg
       }
-    elsif @cuser.flag?
+    elsif @cuser.flag == 4
+      #TODO
+    elsif @cuser.flag? && @user_msg != "홈으로"
       @result = {
-        message: @return_msg,
-        keyboard: @return_keyboard
+        message: @return_msg
       }
     else
       @result = {
