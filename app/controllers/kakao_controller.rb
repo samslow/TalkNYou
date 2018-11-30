b5602a38a8eca358527d9d62c36350c5fd620a00
kitty-700 <31284335+kitty-700@users.noreply.github.com>
2018-12-01 04:51:34 +0900

    F0, F10, F15 시도

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -55,6 +55,7 @@ OPERATION_SENTENCE = #버튼을 통해 클라이언트에서 서버로 입력되
 	OP_PRINT_SITE_LIST = "[사이트 리스트 보기]",
 	OP_ADD_SITE = "[사이트 추가]",
 	OP_ADD_ACCOUNT = "[계정 추가]"
+	OP_INPUT_CANCEL = "-1"
 ]
 # MINE FLAG ENUM SET
 # 여기서의 플래그 이름은 모두 이벤트가 일어난 이후를 설명한다.
@@ -89,13 +90,14 @@ FLAG_ENUM =
 #	when [현재 상태 내용 1(영어)]
 #		├	case @msg_from_user
 #		├	when [OP_명령어 1] #메뉴가 정확히 주어졌을 경우 (예를 들어 사이트 추가나 계정 추가를 클릭했을 경우)
-#		├	┼	...
-#		├	┼	[보낼 텍스트 및 버튼 리스트 추가]  (버튼은 push_**** 함수를 통해 추가한다.)
-#		├	┴	state_transition(@talking_user.flag, [전이될 상태 내용(영어)])
+#		├	┼	(처리)
+#		├	┼	(메세지 생성) [보낼 텍스트 및 버튼 리스트 추가]  (버튼은 push_**** 함수를 통해 추가한다.)
+#		├	┴	(상태전이) state_transition(@talking_user.flag, [전이될 상태 내용(영어)])
 #		├	when [OP_명령어 2] .....(처리, 메세지 생성, 상태전이).......
 #		├	when [OP_명령어 3] .....(처리, 메세지 생성, 상태전이).......
 #		├	when [OP_명령어 4] .....(처리, 메세지 생성, 상태전이).......
 #		├	else #메뉴가 정확히 주어지지 않은 경우 (예를 들어 특정 계정이나 특정 사이트를 클릭했을 경우)  .....(처리, 메세지 생성, 상태전이).......
+#		├		.....(처리, 메세지 생성, 상태전이).......
 #		└	end
 #	when [현재 상태 내용 2(영어)] ............
 #	when [현재 상태 내용 3(영어)] ............
@@ -108,7 +110,7 @@ case @talking_user.flag
 # F00 : 홈 메뉴 => F10 : 사이트 목록 출력
 when HOME_MENU
 	case @msg_from_user
-	when OP_PRINT_SITE_LIST
+	when OP_PRINT_SITE_LIST #-> 이 버튼을 클릭했을 때 띄워줘야 할 다음 텍스트와 버튼들은?
 		@text = "사이트 리스트입니다."
 		push_site_list()
 		push_string(OP_ADD_SITE)
@@ -121,50 +123,170 @@ end
 when PRINT_SITE_LIST
 	case @msg_from_user
 	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
 		state_transition(@talking_user.flag, HOME_MENU)
 	when OP_ADD_SITE
 		@text = "새 사이트의 이름을 입력해주세요."
 		state_transition(@talking_user.flag, ADD_SITE)
 		#이후 키보드 입력을 기다린다.
-
-	
 	else #만약 들어온 입력이 이미 존재하는 사이트 이름이면? F20 : 계정 목록 출력으로 전이
 	#메뉴가 정확히 주어지지 않은 경우 (예를 들어 계정목록이나 사이트목록을 클릭했을 경우 -> 맨 뒤의 코딩템플릿 참조)
 		
-		state_transition(@talking_user.flag, PRINT_ACCOUNT_LIST)
+		@text = "원래는 이 사이트의 계정들이 나와야하는데 아직 구현안됐습니다. 다시 홈으로"
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+		#state_transition(@talking_user.flag, PRINT_ACCOUNT_LIST)
 	end
 end
-# F15 : 사이트 추가
-when 10
+# F15 : 사이트 추가 (버튼이 아닌 텍스트로 입력받는다.)
+when ADD_SITE
+	case @msg_from_user
+	when OP_INPUT_CANCEL
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		if @talking_user.sites.find_by(site_name: @msg_from_user) #이미 존재하면
+			@text = "이미 존재하는 사이트라서 새로 추가하진 않았습니다."
+		else
+			Site.create(site_name: @user_msg, user: @talking_user)
+			@text = @msg_from_user + "추가 완료!"
+		end
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F16 : 사이트 이름 변경
-when 10
+when UPDATE_SITE_NAME
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
+
 # F19 : 삭제 (필요 없을수도 -> 누르자마자 지워버릴 수도 있음. 삭제의 경우 제약조건 없음)
-when 10
+when DELETE_SITE
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 
 # F20 : 계정 목록 출력
-when 10
+when PRINT_ACCOUNT_LIST
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F21 : 개별 계정 메뉴 출력
-when 10
+when PRINT_EACH_ACCOUNT
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F23 : 계정 추가 중 ID 입력
-when 10
+when ADD_ACCOUNT_AT_ID
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F24 : 계정 추가 중 PW 입력
-when 10
+when ADD_ACCOUNT_AT_PW
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F25 : 계정 추가 중 MEMO 입력
-when 10
+when ADD_ACCOUNT_AT_MEMO
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F26 : 계정 변경 중 ID 변경
-when 10
+when UPDATE_ACCOUNT_AT_ID
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F26 : 계정 변경 중 PW 변경
-when 10
+when UPDATE_ACCOUNT_AT_PW
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F26 : 계정 변경 중 MEMO 변경
-when 10
+when UPDATE_ACCOUNT_AT_MEMO
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # F29 : 계정 삭제
-when 10
+when DELETE_ACCOUNT
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 
 # F30 :  계정 추가 시 에러
-when 10
+when IDONTKNOW
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 # UNDEFINED CASE => 무조건 홈으로
 else 
-	@talking_user.update(flag: 0)  
+	case @msg_from_user
+	when OP_TO_HOME
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	else
+		push_string(OP_PRINT_SITE_LIST)
+		state_transition(@talking_user.flag, HOME_MENU)
+	end
 end
 
 ##▲상태에 따른 이벤트 처리 방법 기술▲
