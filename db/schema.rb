60dda5ac60fe7e0e7bf5b23cdc4745414f4ad4dd
kitty-700 <31284335+kitty-700@users.noreply.github.com>
2018-12-01 00:41:15 +0900

    User 의 문자열 입력 시 임시 저장을 위한 속성 추가

--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,27 @@
 
 ActiveRecord::Schema.define(version: 20181026173027) do
 #rake db:migrate:reset 을 통해 DB 초기화 & 마이그레이션 재작업
+  create_table "accounts", force: :cascade do |t|
     t.integer  "site_id"
     t.string   "ID_name"
     t.string   "PW"
     t.string   "Memo"
-    t.datetime "updated_at",                   null: false
   end
   
   create_table "sites", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "site_name"
-  end
+  end  
 
   create_table "users", force: :cascade do |t|
     t.string   "key"
     t.integer  "flag",       default: 0 #기본 플래그는 0 -> 홈메뉴
+
+	t.string  "str_1" #이 str_n 들은 사용자의 입력을 저장하는 용도
+	t.string  "str_2"
+	t.string  "str_3"
+	t.string  "str_4"
+	t.string  "str_5"
   end
 
 end
