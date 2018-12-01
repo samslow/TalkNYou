dd452e4b556a9f0bcd70a52c81cff2570bf53ebd
kitty-700 <31284335+kitty-700@users.noreply.github.com>
2018-12-01 19:10:48 +0900

    코드 위치 변경 및 주석 재작업

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,57 +1,17 @@
 class KakaoController < ApplicationController
- def keyboard
-    @keyboard = {
-      type: "buttons",
-      :buttons => ["사이트 리스트"]
-    }
-    render json: @keyboard
-  end
-
-  def message
-    if User.find_by(key: params[:user_key])
-      p "이미 DB에 존재하는 유저이다."
-    else
-      p "DB에 존재하지 않는 유저이므로 생성 시도 중 . . ."
-      if User.create(key: params[:user_key])
-        p "생성 성공"
-      end
-    end
-	  #keyboard 메소드와 message 메소드는 기본적으로 필요하다. 
-
-
-    @msg_from_user = params[:content] #사용자의 입력값
-    @talking_user = User.find_by(key: params[:user_key]) #Users 테이블에서 User 객체 하나를 찾는다.
-    @button_list = [] #클라이언트에게 출력할 버튼들의 리스트
-    
-    def push_site_list #button_list 에 사이트 목록을 넣는다.
-	 @talking_user.sites.each do |each_site|
-	  # if not @button_list.include?(each_site.site_name)  #이 코드는 이전 스키마의 한계점인 중복사이트 가능성 때문인 것으로 추정
-		push_string(each_site.site_name)
-	  end
-	 end
-	 
-	 def push_string(any_string) #button_list 에 아무 문자열이나 넣는다.
-	  if(any_string.kind_of?(String))
-	   @button_list.push(any_string)
-	  else
-	   "문자열 말고 다른거 넣지 마세요."
-	  end
-	 end
-
-	 def state_transition(now_state, to_be_state) #현재 상태와 전이될 상태를 체크하고 적절하면 전이 수행, 부적절하면 에러 띄우고 홈메뉴로.
-		#근데 우선 일단은 바로 전이만 되게 한다.
-		@talking_user.update(flag: to_be_state)
+	def keyboard
+		@keyboard = {
+		type: "buttons",
+		:buttons => ["사이트 리스트"]
+		}
+		render json: @keyboard
 	end
+	
 
-	 def to_home # F0 : 홈 메뉴로 돌아간다. 다만 호출 전에 진행 중인 작업을 정상적으로 종료할 것
-		@talking_user.update(flag: HOME_MENU)
-	 end
-    #@talking_user.flag 란 대화중인 유저의 상태번호를 나타내는 정수
-	#text 란 서버에서 클라이언트로 보낼 문자열
-	#msg_from_user 란 클라이언트가 서버에 전달할 메세지 (주로 버튼 혹은 문자열 입력을 통해) 
-
+##############▼ 상수 집합 ▼##############
+#상수 선언하는 방법 : 식별자를 대문자로.
 #버튼을 통해 클라이언트에서 서버로 입력되는 명령 문자열 집합
-#상수로 선언하는 방법 : 식별자를 대문자로.
+
 	OP_TO_HOME = "[홈으로]"
 	OP_PRINT_SITE_LIST = "[사이트 리스트 보기]"
 	OP_ADD_SITE = "[사이트 추가]"
@@ -60,8 +20,8 @@ class KakaoController < ApplicationController
 
 # 여기서의 플래그 이름은 모두 이벤트가 일어난 이후를 설명한다.
 # 예를 들어, F10 : 사이트 목록 출력은 이미 사이트 목록이 출력된 이후의 상태를 나타낸다.
-	HOME_MENU = 0					# F00 : 홈 메뉴
-	PRINT_SITE_LIST = 10			# F10 : 사이트 목록 출력
+	HOME_MENU=0					# F00 : 홈 메뉴
+	PRINT_SITE_LIST=10			# F10 : 사이트 목록 출력
 	ADD_SITE = 15						# F15 : 사이트 추가
 	UPDATE_SITE_NAME = 16		# F16 : 사이트 이름 변경
 	DELETE_SITE = 19					# F19 : 삭제 (필요 없을수도 -> 누르자마자 지워버릴 수도 있음. 삭제의 경우 제약조건 없음)
@@ -77,6 +37,55 @@ class KakaoController < ApplicationController
 	DELETE_ACCOUNT = 29					# F29 : 계정 삭제
 
 	IDONTKNOW = 30 	# F30 :  계정 추가 시 에러
+##############▲ 상수 집합 ▲##############
+
+	def push_site_list #button_list 에 사이트 목록을 넣는다.
+		@talking_user.sites.each do |each_site|
+		# if not @button_list.include?(each_site.site_name)  #이 코드는 이전 스키마의 한계점인 중복사이트 가능성 때문인 것으로 추정
+			push_string(each_site.site_name)
+		end
+	end
+ 
+	def push_string(any_string) #button_list 에 아무 문자열이나 넣는다.
+		if(any_string.kind_of?(String))
+			@button_list.push(any_string)
+		else
+			"문자열 말고 다른거 넣지 마세요."
+		end
+	end
+
+	def state_transition(now_state, to_be_state) #현재 상태와 전이될 상태를 체크하고 적절하면 전이 수행, 부적절하면 에러 띄우고 홈메뉴로.
+		#근데 우선 일단은 바로 전이만 되게 한다.
+		@talking_user.update(flag: to_be_state)
+	end
+
+	def to_home # F0 : 홈 메뉴로 돌아간다. 다만 호출 전에 진행 중인 작업을 정상적으로 종료할 것
+		@talking_user.update(flag: HOME_MENU)
+	end
+
+	def message #이 메소드가 전부다.
+		if User.find_by(key: params[:user_key])
+			p "이미 DB에 존재하는 유저이다."
+		else
+			p "DB에 존재하지 않는 유저이므로 생성 시도 중 . . ."
+			if User.create(key: params[:user_key])
+				p "생성 성공"
+			end
+		end
+	  #keyboard 메소드와 message 메소드는 기본적으로 필요하다. 
+
+
+		@msg_from_user = params[:content] 
+#msg_from_user 란 클라이언트가 서버에 전달할 메세지 (주로 버튼 혹은 문자열 입력을 통해) 
+
+		@talking_user = User.find_by(key: params[:user_key]) #Users 테이블에서 User 객체 하나를 찾는다.
+#@talking_user.flag 란 대화중인 유저의 상태번호를 나타내는 정수
+
+		@button_list = [] 
+#@button_list 란 클라이언트에게 출력할 버튼들의 리스트
+
+		@text = ""
+#text 란 서버에서 클라이언트로 보낼 텍스트
 
 ################################
 ##▼상태에 따른 이벤트 처리 방법 기술▼
@@ -138,9 +147,9 @@ when ADD_SITE
 	case @msg_from_user
 	when OP_INPUT_CANCEL
 		push_string(OP_PRINT_SITE_LIST)
-		state_transition(@talking_user.flag, HOME_MENU)
+	state_transition(@talking_user.flag, HOME_MENU)
 	else
-		if @talking_user.sites.find_by(site_name: @msg_from_user) #이미 존재하면
+	if @talking_user.sites.find_by(site_name: @msg_from_user) #이미 존재하면
 			@text = "이미 존재하는 사이트라서 새로 추가하진 않았습니다."
 		else
 			Site.create(site_name: @user_msg, user: @talking_user)
