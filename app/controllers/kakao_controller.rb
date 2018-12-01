41847b989b77217b815d4ff7ccac044aa4d90a9c
kitty-700 <31284335+kitty-700@users.noreply.github.com>
2018-12-01 18:29:31 +0900

    열거형 포기하고 상수로 바꾼다.

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -49,39 +49,36 @@ class KakaoController < ApplicationController
     #@talking_user.flag 란 대화중인 유저의 상태번호를 나타내는 정수
 	#text 란 서버에서 클라이언트로 보낼 문자열
 	#msg_from_user 란 클라이언트가 서버에 전달할 메세지 (주로 버튼 혹은 문자열 입력을 통해) 
-OPERATION_SENTENCE = #버튼을 통해 클라이언트에서 서버로 입력되는 명령 문자열 집합
-[
-	OP_TO_HOME = "[홈으로]",
-	OP_PRINT_SITE_LIST = "[사이트 리스트 보기]",
-	OP_ADD_SITE = "[사이트 추가]",
-	OP_ADD_ACCOUNT = "[계정 추가]",
+
+#버튼을 통해 클라이언트에서 서버로 입력되는 명령 문자열 집합
+#상수로 선언하는 방법 : 식별자를 대문자로.
+	OP_TO_HOME = "[홈으로]"
+	OP_PRINT_SITE_LIST = "[사이트 리스트 보기]"
+	OP_ADD_SITE = "[사이트 추가]"
+	OP_ADD_ACCOUNT = "[계정 추가]"
 	OP_INPUT_CANCEL = "-1"
-]
-# MINE FLAG ENUM SET
+
 # 여기서의 플래그 이름은 모두 이벤트가 일어난 이후를 설명한다.
 # 예를 들어, F10 : 사이트 목록 출력은 이미 사이트 목록이 출력된 이후의 상태를 나타낸다.
-FLAG_ENUM =
-[ 
-  HOME_MENU = 0,					# F00 : 홈 메뉴
-  PRINT_SITE_LIST = 10,			# F10 : 사이트 목록 출력
-  ADD_SITE = 15,						# F15 : 사이트 추가
-  UPDATE_SITE_NAME = 16,		# F16 : 사이트 이름 변경
-  DELETE_SITE = 19,					# F19 : 삭제 (필요 없을수도 -> 누르자마자 지워버릴 수도 있음. 삭제의 경우 제약조건 없음)
+	HOME_MENU = 0					# F00 : 홈 메뉴
+	PRINT_SITE_LIST = 10			# F10 : 사이트 목록 출력
+	ADD_SITE = 15						# F15 : 사이트 추가
+	UPDATE_SITE_NAME = 16		# F16 : 사이트 이름 변경
+	DELETE_SITE = 19					# F19 : 삭제 (필요 없을수도 -> 누르자마자 지워버릴 수도 있음. 삭제의 경우 제약조건 없음)
 
-  PRINT_ACCOUNT_LIST = 20,				# F20 : 계정 목록 출력
-  PRINT_EACH_ACCOUNT = 21,			# F21 : 개별 계정 메뉴 출력
-  ADD_ACCOUNT_AT_ID = 23,				# F23 : 계정 추가 중 ID 입력
-  ADD_ACCOUNT_AT_PW = 24,			# F24 : 계정 추가 중 PW 입력
-  ADD_ACCOUNT_AT_MEMO = 25,		# F25 : 계정 추가 중 MEMO 입력
-  UPDATE_ACCOUNT_AT_ID = 26,		# F26 : 계정 정보 중 ID 변경
-  UPDATE_ACCOUNT_AT_PW = 27,		# F27 : 계정 정보 중 PW 변경
-  UPDATE_ACCOUNT_AT_MEMO = 28,	# F28 : 계정 정보 중 MEMO 변경
-  DELETE_ACCOUNT = 29,					# F29 : 계정 삭제
+	PRINT_ACCOUNT_LIST = 20				# F20 : 계정 목록 출력
+	PRINT_EACH_ACCOUNT = 21			# F21 : 개별 계정 메뉴 출력
+	ADD_ACCOUNT_AT_ID = 23				# F23 : 계정 추가 중 ID 입력
+	ADD_ACCOUNT_AT_PW = 24			# F24 : 계정 추가 중 PW 입력
+	ADD_ACCOUNT_AT_MEMO = 25		# F25 : 계정 추가 중 MEMO 입력
+	UPDATE_ACCOUNT_AT_ID = 26		# F26 : 계정 정보 중 ID 변경
+	UPDATE_ACCOUNT_AT_PW = 27		# F27 : 계정 정보 중 PW 변경
+	UPDATE_ACCOUNT_AT_MEMO = 28	# F28 : 계정 정보 중 MEMO 변경
+	DELETE_ACCOUNT = 29					# F29 : 계정 삭제
 
-  IDONTKNOW = 30 ]	# F30 :  계정 추가 시 에러
+	IDONTKNOW = 30 	# F30 :  계정 추가 시 에러
 
 ################################
-
 ##▼상태에 따른 이벤트 처리 방법 기술▼
 #코딩 템플릿 
 #주의할 점: 상태마다 유효한 명령어가 다르다.	
